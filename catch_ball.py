3732bfb4f4b35e4d55a83d1746aaecc8e0b00e18
kunigaku <kunigaku@gmail.com>
2018-01-04 00:29:04 +0900

    Rewrite with Keras-RL
    https://github.com/matthiasplappert/keras-rl

--- catch_ball.py
+++ catch_ball.py
@@ -1,8 +1,17 @@
 import os
+import gym
+import gym.spaces
 import numpy as np
 
+import matplotlib.pyplot as plt
 
-class CatchBall:
+
+class CatchBall(gym.core.Env):
+    metadata = {
+        'render.modes': ['human'],
+        'video.frames_per_second': 50
+    }
+
     def __init__(self):
         # parameters
         self.name = os.path.splitext(os.path.basename(__file__))[0]
@@ -12,8 +21,37 @@ class CatchBall:
         self.enable_actions = (0, 1, 2)
         self.frame_rate = 5
 
-        # variables
-        self.reset()
+        # 行動空間。何もしない、左、右の3種
+        self.action_space = gym.spaces.Discrete(3)
+        # 観測空間(state)の次元 (2次元スクリーン) とその最大値
+        self.observation_space = gym.spaces.Box(
+            0.0, 1.0, (self.screen_n_rows, self.screen_n_cols))
+
+        self.figure = None
+
+        # 使うメンバ変数を初期化
+        self.reward = 0
+        self.screen = np.zeros((self.screen_n_rows, self.screen_n_cols))
+        self.rgb_screen = np.zeros((self.screen_n_rows, self.screen_n_cols, 3))
+        self.terminal = False
+        self.player_row = self.screen_n_rows - 1
+        self.player_col = np.random.randint(
+            self.screen_n_cols - self.player_length)
+        self.ball_row = 0
+        self.ball_col = np.random.randint(self.screen_n_cols)
+
+    # 各stepごとに呼ばれる
+    # actionを受け取り、次のstateとreward、episodeが終了したかどうかを返すように実装
+    def _step(self, action):
+        """
+        action:
+            0: do nothing
+            1: move left
+            2: move right
+        """
+        self.update(action)
+        self.draw()
+        return self.screen, self.reward, self.terminal, {}
 
     def update(self, action):
         """
@@ -28,7 +66,8 @@ class CatchBall:
             self.player_col = max(0, self.player_col - 1)
         elif action == self.enable_actions[2]:
             # move right
-            self.player_col = min(self.player_col + 1, self.screen_n_cols - self.player_length)
+            self.player_col = min(self.player_col + 1,
+                                  self.screen_n_cols - self.player_length)
         else:
             # do nothing
             pass
@@ -49,26 +88,22 @@ class CatchBall:
                 self.reward = -1
 
     def draw(self):
+        "update state"
         # reset screen
         self.screen = np.zeros((self.screen_n_rows, self.screen_n_cols))
 
         # draw player
-        self.screen[self.player_row, self.player_col:self.player_col + self.player_length] = 1
+        self.screen[self.player_row,
+                    self.player_col:self.player_col + self.player_length] = 1
 
         # draw ball
         self.screen[self.ball_row, self.ball_col] = 1
 
-    def observe(self):
-        self.draw()
-        return self.screen, self.reward, self.terminal
-
-    def execute_action(self, action):
-        self.update(action)
-
-    def reset(self):
+    def _reset(self):
         # reset player position
         self.player_row = self.screen_n_rows - 1
-        self.player_col = np.random.randint(self.screen_n_cols - self.player_length)
+        self.player_col = np.random.randint(
+            self.screen_n_cols - self.player_length)
 
         # reset ball position
         self.ball_row = 0
@@ -77,3 +112,17 @@ class CatchBall:
         # reset other variables
         self.reward = 0
         self.terminal = False
+        self.draw()
+        return self.screen
+
+    def _render(self, mode='human', close=False):
+        if close:
+            return
+        if self.figure is None:
+            # animate
+            self.figure = plt.figure(
+                figsize=(self.screen_n_rows / 2, self.screen_n_cols / 2))
+            self.image = plt.imshow(
+                self.screen, interpolation="none", cmap="gray")
+        self.image.set_array(self.screen)
+        plt.pause(.01)
---
+++ dqn_model.py
@@ -0,0 +1,36 @@
+import numpy as np
+import gym
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.optimizers import Adam
+
+from rl.agents.dqn import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+
+
+def get_dqn_model(env):
+    "dqn model for catch_ball"
+    nb_actions = env.action_space.n
+    # Next, we build a very simple model.
+    model = Sequential()
+    model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dense(32))
+    model.add(Activation('relu'))
+    model.add(Dense(16))
+    model.add(Activation('relu'))
+    model.add(Dense(nb_actions))
+    model.add(Activation('linear'))
+    print(model.summary())
+
+    # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
+    # even the metrics!
+    memory = SequentialMemory(limit=50000, window_length=1)
+    policy = BoltzmannQPolicy()
+    dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=100,
+                   target_model_update=1e-2, policy=policy)
+    dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+    return dqn
